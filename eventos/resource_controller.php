47c25984917e1075c66bcd64da237c316595eca4
Matt-21 <m.lopes.1795@gmail.com>
2016-10-04 00:27:40 -0300

    Updates db_connections

---
+++ eventos/resource_controller.php
@@ -0,0 +1,47 @@
+<?php
+
+include_once ('request.php');
+include_once ('db_user.php');
+include_once ('db_events.php');
+include_once ('db_images.php');
+
+class ResourceController
+{	
+ 	private $METHODMAP = ['GET' => 'search' , 'POST' => 'create' , 'PUT' => 'update', 'DELETE' => 'remove' ];
+	
+	public function treat_request($request) {
+		return $this->{$this->METHODMAP[$request->getMethod()]}($request);
+	
+	}
+
+	private function search($request) {
+	   $query ='SELECT * FROM '.$request->getResource().' WHERE '.self::queryParams($request->getParameters());
+		return self::random($query); 
+	}
+
+	private function EventsQuery($request){
+		$connector =(new DB_Events())->query($sql);
+		$sql = 'SELECT * FROM '.$request->getResource().' WHERE '.self::queryParams($request->getParameters());
+		return $connector->fetchObject(fetchAll());	
+	}
+		
+	private function random($query){
+	  $result = (new DBConnector())->query($query);
+	    return $result->fetchObject(fetchAll());
+	}
+	
+	private function queryParams($params) {
+		$query = "";		
+		foreach($params as $key => $value) {
+			$query .= $key.' = '.$value.' AND ';	
+		}
+		$query = substr($query,0,-5);
+		return $query;
+	}
+
+	
+}
+
+
+
+
